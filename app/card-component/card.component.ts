84c0be4f0a64cb5a6d8b3979fe13ec96cffd268d
Andre <harder@posteo.de>
2016-04-24 14:35:21 +0200

    message card component introduced

---
+++ app/card-component/card.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input} from 'angular2/core';
+
+@Component({
+	selector: 'message-card',
+	templateUrl: './app/card-component/card.component.html'
+})
+
+export class MessageCardComponent {
+	@Input() message;
+}
--- app/retro-board/retro.board.ts
+++ app/retro-board/retro.board.ts
@@ -1,19 +1,24 @@
 import {Component, OnInit} from 'angular2/core';
 import {RetroService} from '../retro.service';
-
+import {MessageCardComponent} from '../card-component/card.component';
 
 @Component({
 	selector: 'retro-board',
-	templateUrl: './app/retro-board/retro.board.html'
+	templateUrl: './app/retro-board/retro.board.html',
+	directives: [MessageCardComponent]
 })
 
 export class RetroBoard implements OnInit{
-
+	public messages: string[]= [];
 	public topic:String;
 
 	constructor(private _retroService: RetroService) {}
 
 	ngOnInit(){
 		this.topic = this._retroService.getTopic();
+	}
+
+	addMessage(msg: string){
+		this.messages.push(msg);
 	}
 }
